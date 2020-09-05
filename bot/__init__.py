0da42f6c9335ba432bf1173565a61280db0cf6c0
Frizzy | ‎미나 <dhanikata@gmail.com>
2020-09-06 01:49:24 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -161,6 +161,15 @@ try:
         BLOCK_MEGA_LINKS = False
 except KeyError:
     BLOCK_MEGA_LINKS = False
+    
+try:
+    SHORTENER = getConfig('SHORTENER')
+    SHORTENER_API = getConfig('SHORTENER_API')
+    if len(SHORTENER) == 0 or len(SHORTENER_API) == 0:
+        raise KeyError
+except KeyError:
+    SHORTENER = None
+    SHORTENER_API = None
 
 updater = tg.Updater(token=BOT_TOKEN,use_context=True)
 bot = updater.bot
